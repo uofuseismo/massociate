c516dd584ffcc8ca9d18f5e728764196bea9ac76
Ben Baker <bakerb845@gmail.com>
2021-11-30 12:24:57 -0700

    updating cmakemodules to deal with newer versions of cmake policies and oneapi

--- CMakeModules/FindMKLConfig.cmake
+++ CMakeModules/FindMKLConfig.cmake
@@ -17,11 +17,13 @@ endif()
 find_path(MKL_INCLUDE_DIR
           NAMES mkl.h
           HINTS /opt/intel/mkl/include
+                /opt/intel/oneapi/mkl/latest/include/
                 $ENV{MKL_ROOT}/include)
 # Find the library components
 find_library(MKL_INTEL_LIBRARY
              NAMES ${INTEL}
-             PATHS /opt/intel/mkl/lib/intel64
+             PATHS /opt/intel/oneapi/mkl/latest/lib/intel64
+                   /opt/intel/mkl/lib/intel64
                    /opt/intel/mkl/lib 
                    $ENV{MKL_ROOT}/lib/intel64
                    $ENV{MKL_ROOT}/lib
@@ -29,7 +31,8 @@ find_library(MKL_INTEL_LIBRARY
             )
 find_library(MKL_SEQUENTIAL_LIBRARY
              NAMES ${SEQUENTIAL}
-             PATHS /opt/intel/mkl/lib/intel64
+             PATHS /opt/intel/oneapi/mkl/latest/lib/intel64
+                   /opt/intel/mkl/lib/intel64
                    /opt/intel/mkl/lib
                    $ENV{MKL_ROOT}/lib/intel64
                    $ENV{MKL_ROOT}/lib
@@ -37,7 +40,8 @@ find_library(MKL_SEQUENTIAL_LIBRARY
             )
 find_library(MKL_CORE_LIBRARY
              NAMES ${CORE}
-             PATHS /opt/intel/mkl/lib/intel64
+             PATHS /opt/intel/oneapi/mkl/latest/lib/intel64
+                   /opt/intel/mkl/lib/intel64
                    /opt/intel/mkl/lib
                    $ENV{MKL_ROOT}/lib/intel64
                    $ENV{MKL_ROOT}/lib
@@ -49,5 +53,5 @@ set(MKL_LIBRARY ${MKL_INTEL_LIBRARY} ${MKL_SEQUENTIAL_LIBRARY} ${MKL_CORE_LIBRAR
 # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
 # all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MKL DEFAULT_MSG MKL_INCLUDE_DIR MKL_LIBRARY MKL_INTEL_LIBRARY MKL_SEQUENTIAL_LIBRARY MKL_CORE_LIBRARY)
+find_package_handle_standard_args(FindMKL DEFAULT_MSG MKL_INCLUDE_DIR MKL_LIBRARY MKL_INTEL_LIBRARY MKL_SEQUENTIAL_LIBRARY MKL_CORE_LIBRARY)
 mark_as_advanced(MKL_INCLUDE_DIR MKL_LIBRARY)
--- CMakeModules/FindTBBConfig.cmake
+++ CMakeModules/FindTBBConfig.cmake
@@ -7,12 +7,14 @@ set(TBB "tbb")
 
 find_path(TBB_INCLUDE_DIR
           NAMES tbb
-          HINTS /opt/intel/tbb/include
+          HINTS /opt/intel/oneapi/tbb/latest/include
+                /opt/intel/tbb/include
                 $ENV{TBB_ROOT}/include
                 $ENV{TBB_INC_DIR})
 find_library(TBB_LIBRARY
              NAMES ${TBB}
-             PATHS /opt/intel/tbb/lib/intel64
+             PATHS /opt/intel/oneapi/tbb/latest/lib/intel64/gcc4.8/
+                   /opt/intel/tbb/lib/intel64
                    /opt/intel/tbb/lib
                    $ENV{TBB_ROOT}/lib/intel64
                    $ENV{TBB_ROOT}/lib/
@@ -23,5 +25,5 @@ find_library(TBB_LIBRARY
 # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
 # all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(TBB DEFAULT_MSG TBB_LIBRARY TBB_INCLUDE_DIR)
+find_package_handle_standard_args(FindTBB DEFAULT_MSG TBB_LIBRARY TBB_INCLUDE_DIR)
 mark_as_advanced(TBB_INCLUDE_DIR TBB_LIBRARY)
