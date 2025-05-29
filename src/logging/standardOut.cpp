67d4b9d45389c678d5f5692ea701b7cd36eddf8d
Ben Baker <bakerb845@gmail.com>
2025-05-29 14:12:00 -0600

    adding workaround for no umps

---
+++ src/logging/standardOut.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include "standardOut.hpp"
+
+using namespace UMPS::Logging;
+
+class StandardOut::StandardOutImpl
+{
+public:
+    StandardOutImpl() :
+        mStandardOutSink(std::make_shared<spdlog::sinks::stdout_color_sink_mt> ()),
+        mLogger(std::make_shared<spdlog::logger> ("stdout", mStandardOutSink))
+    {
+//        mLogger = spdlog::create("stdout", mStandardOutSink);
+    }
+    //StandardOutImpl() :
+    //    mSink(std::make_shared<spdlog::sinks::stdout_color_sink_mt>() )
+    //{   
+    //}
+    //std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> mSink;
+    Level mLevel = Level::Info;
+    std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> mStandardOutSink{nullptr};
+    std::shared_ptr<spdlog::logger> mLogger = nullptr;
+};
+
+/// C'tor
+StandardOut::StandardOut(const Level level) :
+    pImpl(std::make_unique<StandardOutImpl> ())
+{
+    setLevel(level);
+}
+
+/// Copy c'tor
+StandardOut::StandardOut(const StandardOut &logger)
+{
+    *this = logger;
+}
+
+/// Move c'tor
+StandardOut::StandardOut(StandardOut &&logger) noexcept
+{
+    *this = std::move(logger);
+}
+
+/// Copy assignment
+StandardOut& StandardOut::operator=(const StandardOut &logger)
+{
+    if (&logger == this){return *this;}
+    pImpl = std::make_unique<StandardOutImpl> (*logger.pImpl);
+    return *this;
+}
+
+/// Move assignment
+StandardOut& StandardOut::operator=(StandardOut &&logger) noexcept
+{
+    if (&logger == this){return *this;}
+    pImpl = std::move(logger.pImpl);
+    return *this;
+}
+
+/// Destructor
+StandardOut::~StandardOut() = default;
+
+/// Level
+void StandardOut::setLevel(const Level level) noexcept
+{
+    pImpl->mLevel = level;
+    if (level == Level::Error)
+    {
+        pImpl->mLogger->set_level(spdlog::level::err);
+    }
+    else if (level == Level::Warn)
+    {
+        pImpl->mLogger->set_level(spdlog::level::warn);
+    }
+    else if (level == Level::Info)
+    {
+        pImpl->mLogger->set_level(spdlog::level::info);
+    }
+    else if (level == Level::Debug)
+    {
+        pImpl->mLogger->set_level(spdlog::level::debug);
+    }
+    else
+    {
+        pImpl->mLogger->set_level(spdlog::level::off);
+    }
+}
+
+Level StandardOut::getLevel() const noexcept
+{   
+    return pImpl->mLevel;
+}
+
+/// Info
+void StandardOut::info(const std::string &message)
+{
+    if (pImpl->mLevel >= Level::Info)
+    {
+        //std::cout << message << std::endl;
+        //spdlog::info(message);
+        pImpl->mLogger->info(message);
+    }
+}
+
+/// Warn
+void StandardOut::warn(const std::string &message)
+{
+    if (pImpl->mLevel >= Level::Warn)
+    {
+        //std::cout << message << std::endl;
+        //spdlog::warn(message);
+        pImpl->mLogger->warn(message);
+    }
+}
+
+/// Error
+void StandardOut::error(const std::string &message)
+{
+    if (pImpl->mLevel >= Level::Error)
+    {
+        //std::cerr << message << std::endl;
+        //spdlog::error(message);
+        pImpl->mLogger->error(message);
+    }
+}
+
+/// Debug
+void StandardOut::debug(const std::string &message)
+{
+    if (pImpl->mLevel >= Level::Debug)
+    {
+        //std::cerr << message << std::endl;
+        //spdlog::debug(message);
+        pImpl->mLogger->debug(message);
+    }
+}
---
+++ src/logging/standardOut.hpp
@@ -0,0 +1,59 @@
+#ifndef ULOCATOR_UMPS_LOGGING_STANDARD_OUT_HPP
+#define ULOCATOR_UMPS_LOGGING_STANDARD_OUT_HPP
+#include <memory>
+#include "log.hpp"
+namespace UMPS::Logging
+{
+/// StandardOut "stdout.hpp" "umps/logging/stdout.hpp"
+/// @brief A naive logger that prints all messages to standard out and,
+///        in the case of errors, standard error.
+/// @note This class is not recommended for typical use.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT license.
+/// @ingroup Logging_Loggers
+class StandardOut : public ILog
+{
+public:
+    /// @brief Constructor.
+    explicit StandardOut(Logging::Level level = Logging::Level::Info);
+    /// @brief Copy constructor.
+    StandardOut(const StandardOut &logger);
+    /// @brief Move constructor.
+    StandardOut(StandardOut &&logger) noexcept;
+
+    /// @brief Copy assignment operator.
+    /// @param[in] logger   The logging class to copy to this.
+    /// @result A deep copy of the logger class.
+    StandardOut &operator=(const StandardOut &logger); 
+    /// @brief Move assignment operator.
+    /// @param[in,out] logger  The logging class whose memory will be moved to
+    ///                        this.  On exit, logger's behavior is undefined.
+    /// @result The memory from logger moved to this.
+    StandardOut &operator=(StandardOut &&logger) noexcept;
+
+    /// @brief Destructor.
+    ~StandardOut() override;
+
+    /// @brief Sets the logging level.
+    /// @param[in] level  The logging level.
+    void setLevel(Level level) noexcept;
+    /// @result Gets the logging level.
+    [[nodiscard]] Level getLevel() const noexcept override;
+
+    /// @brief Writes an error message.
+    /// @note This requires \c getLevel() be >= Level::ERROR.
+    void error(const std::string &message) override;
+    /// @brief Writes a warning message.
+    /// @note This requires \c getLevel() be >= Level::WARN.
+    void warn(const std::string &message) override;
+    /// @brief Writes an info message.
+    /// @note This requires \c getLevel() be >= least Level::INFO.
+    void info(const std::string &message) override;
+    /// @brief Writes a debug message.
+    /// @note This requires \c getLevel() be >= Level::DEBUG.
+    void debug(const std::string &message) override;
+private:
+    class StandardOutImpl;
+    std::unique_ptr<StandardOutImpl> pImpl; 
+};
+}
+#endif
