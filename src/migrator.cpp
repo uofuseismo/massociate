5fae9ed0d97b5e66e02f4fd150c2c47df6b98dfb
Ben Baker <bakerb845@gmail.com>
2025-05-29 14:21:56 -0600

    adding local logging include

--- src/migrator.cpp
+++ src/migrator.cpp
@@ -10,7 +10,11 @@
 #include <uLocator/travelTimeCalculator.hpp>
 #include <uLocator/position/knownLocalLocation.hpp>
 #include <uLocator/position/geographicRegion.hpp>
+#ifdef WITH_LOCAL_UMPS_LOGGING
+#include "logging/standardOut.hpp"
+#else
 #include <umps/logging/standardOut.hpp>
+#endif
 #include "massociate/migrator.hpp"
 #include "massociate/arrival.hpp"
 #include "massociate/waveformIdentifier.hpp"
